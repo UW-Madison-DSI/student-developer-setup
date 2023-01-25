c4361fec847b81d5055c026856cca7775e23ddc3
littlekimber <zhyan0096@gmail.com>
2023-01-24 20:19:08 -0600

    finish sample python code, pre-commit configuration

---
+++ Zhuang/hello.py
@@ -0,0 +1,14 @@
+"""
+This file is a test file
+"""
+
+
+def hello():
+    """
+    This method prints greeting words to the people who use it
+    :return: None
+    """
+
+
+if __name__ == '__main__':
+    hello()
